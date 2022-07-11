f2ede15fb7419964310461435a0a0199d56a8dec
Andrew Prakash <andrewprakash@Andrews-MacBook-Pro.local>
2022-07-11 13:00:00 +1200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import * as core from '@actions/core';
+import aws from 'aws-sdk';
+
+export const run = async () => {
+    try {
+        const sqsUrl = core.getInput('sqs-url', { required: false });
+        const variables = core.getInput('variables', { required: false });
+
+        const splitVars = variables.toUpperCase().split(",");
+
+        // @ts-ignore
+        const message = splitVars.reduce((prv,crv) => {
+            return {
+              ...prv,
+              [crv]: process.env[crv]
+            }
+          }, {})
+
+        const params = {
+            QueueUrl: sqsUrl,
+            MessageBody: JSON.stringify(message),
+        };
+        
+        const sqs = new aws.SQS();
+        sqs.sendMessage(params, (err, resp) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log(`resp ${JSON.stringify(resp, null, 2)}`);
+            }
+        })
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
